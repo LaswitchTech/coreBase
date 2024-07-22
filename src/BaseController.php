833437e8bab6d0400fddd235e296ab405ecb54ee
Louis Ouellet <louis_ouellet@hotmail.com>
2024-07-22 11:18:16 -0400

    Added the missing import of coreConfigurator

--- src/BaseController.php
+++ src/BaseController.php
@@ -4,6 +4,7 @@
 namespace LaswitchTech\coreBase;
 
 // Import additionnal class into the global namespace
+use LaswitchTech\coreConfigurator\Configurator;
 use LaswitchTech\coreLogger\Logger;
 use LaswitchTech\coreCSRF\CSRF;
 
